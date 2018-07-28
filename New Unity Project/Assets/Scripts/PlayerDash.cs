2b8debc30682e141b13f17ca5f01f71f4bf69570
RMommers <41826773+RMommers@users.noreply.github.com>
2018-07-28 13:37:58 +0200

    bende

---
+++ New Unity Project/Assets/Scripts/PlayerDash.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDash: MonoBehaviour
+{
+
+
+    public float initialMoveSpeed;
+    public float moveSpeed;
+
+    private Animator anim;
+    private Rigidbody2D MyRigidBody;
+    private bool playerMoving;
+    private Vector2 LastMove;
+
+
+
+    //dashing
+    public KeyCode dashButton;
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    private bool Dashing;
+    public float dashCooldown;
+    private float dashCooldownTime;
+    private Vector3 direction;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        MyRigidBody = GetComponent<Rigidbody2D>();
+        dashCooldownTime = 0;
+
+        new Vector3(0f, 0f, 0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(dashButton) == true && dashCooldownTime <= 0)
+        {
+            moveSpeed = dashSpeed;
+            dashTime = startDashTime;
+            dashCooldownTime = dashCooldown;
+        }
+
+        if (dashTime <= 0)
+        {
+            playerMoving = false;
+            moveSpeed = initialMoveSpeed;
+        }
+        else
+        {
+            dashTime -= Time.deltaTime;
+        }
+
+        dashCooldownTime -= Time.deltaTime;
+
+
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            playerMoving = true;
+            LastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        }
+
+
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            MyRigidBody.velocity = new Vector2(MyRigidBody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+            playerMoving = true;
+            LastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        }
+
+        
+            //Stop moving (for Rigidbody)
+            if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+            {
+                MyRigidBody.velocity = new Vector2(0f, MyRigidBody.velocity.y);
+            }
+
+            if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+            {
+                MyRigidBody.velocity = new Vector2(MyRigidBody.velocity.x, 0f);
+            }
+        
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", LastMove.x);
+        anim.SetFloat("LastMoveY", LastMove.y);
+
+    }
+
+}
---
+++ New Unity Project/Assets/Scripts/SlimeController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeController : MonoBehaviour {
+
+    public float MoveSpeed;
+
+    private Rigidbody2D MyRigidBody;
+
+    private bool moving;
+
+    public float timeBetweenMove;
+    private float timeBetweenMoveCounter;
+
+    public float timeToMove;
+    private float timeToMoveCounter;
+
+    private Vector3 moveDirection;
+
+
+	// Use this for initialization
+
+	void Start () {
+        MyRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (moving) {
+            timeToMoveCounter -= Time.deltaTime;
+            MyRigidBody.velocity = moveDirection;
+            if (timeToMoveCounter < 0)
+            {
+                moving = false;
+                //timeBetweenMoveCounter = timeBetweenMove;
+                timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
+
+            }
+
+        }
+        else {
+            timeBetweenMoveCounter -= Time.deltaTime;
+            MyRigidBody.velocity=Vector2.zero;
+
+
+            if(timeBetweenMoveCounter < 0f) {
+                moving = true;
+                //timeToMoveCounter = timeToMove;
+                timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
+                moveDirection = new Vector3(Random.Range(-1f, 1f)*MoveSpeed, Random.Range(-1f, 1f)*MoveSpeed, 0f);
+
+
+            }
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.name == "Player") {
+            Destroy(other.gameObject);
+
+        }
+    }
+}
